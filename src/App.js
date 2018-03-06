bc29f0d0ab23deb95b003ab80b04c5cde5f7e278
hathanhhai <darkwinqw2@gmail.com>
2018-03-06 13:53:29 +0700

    template

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './admins/conpenents/layouts/header'
+import Nav from './admins/conpenents/layouts/nav'
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+            <Header/>
+            <Nav/>
+     
+    </header>
+
+     <main>
+            
+      </main>
+ 
+      </React.Fragment>
+    );
+    }
+  }
+
+export default App;
